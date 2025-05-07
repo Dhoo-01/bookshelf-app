ee3083012edb155652a0b2158c33e3380b523193
Karyawan magang poludnitsa#1 <158252972+Dhoo-01@users.noreply.github.com>
2025-05-07 13:50:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,75 @@
 // Do your work here...
-console.log('Hello, world!');
+console.log("Hello, world!");
 
-  /*{ wajib ditampilkan di html
-    data-bookid: id,
-    data-testid: {
-      bookItem: item,
-      bookItemTitle: title,
-      bookItemAuthor: author,
-      bookItemYear: year,
-      bookItemIsCompleteButton: isComplete,
-      bookItemDeleteButton: delete,
-      bookItemEditButton: edit
-    }
-  } */
-
-let Books = 
-[
+let books = [
   /* {
-    id: String | Number,
-    title: String,
-    author: String,
-    year: Number,
-    isComplete: Boolean
+    id: number | string,
+    title: string,
+    author: string,
+    year: number,
+    isCompleted: boolean
   } */
 ];
 
-const SAVED_EVENT = 'saved-bookshelf';
-const STORAGE_KEY = 'BOOKSHELF_APP';
+const SAVED_EVENT = "saved-book";
+const STORAGE_KEY = "BOOKSHELF_APPS";
 
-//periksa browser support untuk web storage
+// fungsi untuk memeriksa apakah browser support local storage
 function isStorageExist() {
-  // body...
-  if (typeof (Storage) === undefined) {
+  if (typeof Storage === undefined) {
     alert("browser kamu tidak mendukung local storage");
     return false;
   }
+
   return true;
 }
 
-document.addEventListener(SAVED_EVENT, function () {
-  console.log(localStorage.getItem(STORAGE_KEY));
-})
-
-//fungsi untuk simpan ke local storage
+// fungsi untuk menyimpan ke local storage
 function saveData() {
   if (isStorageExist()) {
-    const parsed = JSON.stringify(Books);
+    const parsed = JSON.stringify(books);
     localStorage.setItem(STORAGE_KEY, parsed);
     document.dispatchEvent(new Event(SAVED_EVENT));
   }
 }
+
+function generateId() {
+  return Number(new Date());
+}
+
+function generateBookObject(id, title, author, year, isCompleted) {
+  return {
+    id,
+    title,
+    author,
+    year,
+    isCompleted,
+  };
+}
+
+function addBook() {
+  const id = generateId();
+  const title = document.getElementById("bookFormTitle").value;
+  const author = document.getElementById("bookFormAuthor").value;
+  const year = document.getElementById("bookFormYear").value;
+  const isCompleted = document.getElementById("bookFormIsComplete").checked;
+  // const isCompletedCheck = isCompleted.checked;
+
+  const bookObject = generateBookObject(id, title, author, year, isCompleted);
+  books.push(bookObject);
+
+  document.dispatchEvent(new Event(SAVED_EVENT));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("bookForm");
+  submitForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addBook();
+    submitForm.reset();
+  });
+});
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(books);
+});
