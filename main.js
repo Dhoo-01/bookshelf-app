c2b5c5627cf976c51a0d4b9acc80faf919a0287d
Karyawan magang poludnitsa#1 <158252972+Dhoo-01@users.noreply.github.com>
2025-05-08 16:13:32 +0800

    Add makeBook function

--- main.js
+++ main.js
@@ -72,4 +72,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener(SAVED_EVENT, function () {
   console.log(books);
+  const incompleteBookList = document.getElementById("incompleteBookList");
+  incompleteBookList.innerHTML = "";
+  
+  for (const bookItems of books) {
+    const bookElement = makeBook(bookItems);
+    incompleteBookList.append(bookElement);
+  }
 });
+
+function makeBook(bookObject) {
+  //pembungkus book
+  const bookItem = document.createElement("div");
+  bookItem.setAttribute("class", "bookItem");
+  bookItem.setAttribute("data-bookid", `${bookObject.id}`);
+  bookItem.setAttribute("data-testid", "bookItem");
+  
+  const bookTitle = document.createElement("h3");
+  bookTitle.setAttribute("data-testid", "bookItemTitle");
+  bookTitle.innerText = bookObject.title;
+  
+  const bookAuthor = document.createElement("p");
+  bookAuthor.setAttribute("data-testid", "bookItemAuthor");
+  bookAuthor.innerText = `Penulis:  ${bookObject.author}`;
+
+  const bookYear = document.createElement("p");
+  bookYear.setAttribute("data-testid", "bookItemYear");
+  bookYear.innerText = `Tahun: ${bookObject.year}`;
+  
+  const btnWrap = document.createElement("div");
+  
+  const completeBtn = document.createElement("button");
+  completeBtn.setAttribute("data-testid", "bookItemIsCompleteButton");
+  completeBtn.innerText = "Selesai dibaca";
+  
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("data-testid", "bookItemDeleteButton");
+  deleteBtn.innerText = "Hapus Buku";
+  
+  const editBtn = document.createElement("button");
+  editBtn.setAttribute("data-testid", "bookItemEditButton");
+  editBtn.innerText = "Edit Buku";
+  
+  btnWrap.append(completeBtn, deleteBtn, editBtn);
+  
+  bookItem.append(bookTitle, bookAuthor, bookYear, btnWrap);
+  
+  return bookItem;
+}
